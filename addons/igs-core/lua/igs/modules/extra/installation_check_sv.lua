999a45a769f21f6c415a0ad1c9feeb035914db45
_AMD_ <me@def.pm>
2021-04-16 17:36:46 +0300

    Проверка установки
    Если скрипт распакован, то предупредим о том, что так не будет обновлений

---
+++ addons/igs-core/lua/igs/modules/extra/installation_check_sv.lua
@@ -0,0 +1,31 @@
+local function isMounted(path)
+	return file.Exists(path, "LUA")
+end
+
+local function isWorkshopped(path)
+	return file.Exists("lua/" .. path, "WORKSHOP")
+end
+
+local function isDownloaded(path)
+	return IGS.CODEMOUNT and IGS.CODEMOUNT[path]
+end
+
+
+
+local function isUnpacked(path)
+	return isMounted(path) and not (isWorkshopped(path) or isDownloaded(path))
+end
+
+hook.Add("IGS.Initialized", "installation_check", function()
+	local path = "igs/launcher.lua"
+	if isUnpacked(path) then
+		IGS.print("Похоже, что автодонат распакован в /addons. Автоматические обновления недоступны")
+	end
+end)
+
+
+-- PRINT(file.Find("*", "LUA")) -- mediaplayer, wire
+-- PRINT(file.Find("lua/*", "THIRDPARTY")) -- mediaplayer, wire
+
+
+-- print(isUnpacked("wire/wireshared.lua"))
