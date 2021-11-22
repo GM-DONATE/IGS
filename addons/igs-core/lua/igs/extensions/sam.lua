5d7915b77ccfc16749694f6e1a783cafc4141110
_AMD_ <amd@default.im>
2021-11-22 19:46:21 +0200

    Update sam.lua
    Упрощение и вроде как фикс приоритетов SAM групп

--- addons/igs-core/lua/igs/extensions/sam.lua
+++ addons/igs-core/lua/igs/extensions/sam.lua
@@ -1,48 +1,29 @@
-IGS.ITEMS.SAM = IGS.ITEMS.SAM or {
-	GROUPS = {}
-}
-
 local STORE_ITEM = FindMetaTable("IGSItem")
 
-function STORE_ITEM:SetSAMGroup(sUserGroup, iGroupWeight)
-	self:SetCategory("SAM (Админка)")
-
-	self:SetCanActivate(function(pl) -- global, invDbID
-		if pl:IsUserGroup(sUserGroup) then
-			return "У вас уже действует эта услуга"
-		end
-	end)
-	self:SetInstaller(function(pl)
+function STORE_ITEM:SetSAMGroup(sUserGroup)
+	return self:SetInstaller(function(pl)
 		pl:sam_set_rank(sUserGroup)
-	end)
-	self:SetValidator(function(pl)
-		if pl.IGSSAMWeight then
-			return iGroupWeight < pl.IGSSAMWeight
-		else
-			return  pl:GetUserGroup() == sUserGroup
+	end):AddHook("IGS.PlayerPurchasesLoaded", function(pl, purchases)
+		if CLIENT or not purchases then return end
+
+		local priority_item = self
+
+		for uid in pairs(purchases) do
+			local ITEM = IGS.GetItemByUID(uid)
+			if ITEM:GetMeta("samgroup") and ITEM.id > self.id then
+				priority_item = ITEM
+			end
 		end
-	end)
 
-
-	self.sg_group = self:Insert(IGS.ITEMS.SAM.GROUPS, sUserGroup)
-	return self
+		if priority_item == self then
+			self:Setup(pl)
+			self.igs_active_sam_item = self
+		end
+	end):SetMeta("samgroup", sUserGroup)
 end
 
-if CLIENT then return end
-
-local function checkGroups(pl)
-	local hasAccess = IGS.PlayerHasOneOf(pl, IGS.ITEMS.SAM.GROUPS[ pl:GetUserGroup() ])
-	if hasAccess == nil then return end  -- не отслеживается
-
-	if hasAccess then
-		return -- если имеется хоть одна покупка, то не снимаем права
-	end
-
-	pl:sam_set_rank("user")
-end
-
-hook.Add("IGS.PlayerPurchasesLoaded", "SAMGroups", function(pl)
-	if next(IGS.ITEMS.SAM.GROUPS) then -- группы продаются
-		checkGroups(pl)
+hook.Add("PlayerDisconnected", "IGS_SAM", function(pl)
+	if pl.igs_active_sam_item then
+		pl:sam_set_rank("user")
 	end
 end)
