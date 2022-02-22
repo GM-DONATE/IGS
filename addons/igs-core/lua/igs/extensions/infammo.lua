6b88861616e8c18cec804ce254057cae0fba8e33
_AMD_ <amd@default.im>
2022-02-22 23:58:42 +0200

    :new: ITEM:SetInfAmmo()
    позволяет создавать услугу с бесконечными патронами

---
+++ addons/igs-core/lua/igs/extensions/infammo.lua
@@ -0,0 +1,70 @@
+-- IGS("Бесконечные патроны", "infammo", 100)
+-- 	:SetInfAmmo()
+-- 	:SetTerm(10)
+
+local ITEM = FindMetaTable("IGSItem")
+
+local function setInfAmmo(pl)
+	local weapon = pl:GetActiveWeapon()
+	if not IsValid(weapon) then return end
+
+	local maxClip      = weapon:GetMaxClip1()
+	local maxClip2     = weapon:GetMaxClip2()
+	local primAmmoType = weapon:GetPrimaryAmmoType()
+	local secAmmoType  = weapon:GetSecondaryAmmoType()
+
+	if maxClip == -1 and maxClip2 == -1 then
+		maxClip = 100
+		maxClip2 = 100
+	end
+
+	if maxClip <= 0 and primAmmoType ~= -1 then
+		maxClip = 1
+	end
+
+	if maxClip2 == -1 and secAmmoType ~= -1 then
+		maxClip2 = 1
+	end
+
+	if maxClip > 0 then
+		weapon:SetClip1(maxClip)
+	end
+
+	if maxClip2 > 0 then
+		weapon:SetClip2(maxClip2)
+	end
+
+	if primAmmoType ~= -1 then
+		pl:SetAmmo( maxClip, primAmmoType, true)
+	end
+
+	if secAmmoType ~= -1 and secAmmoType ~= primAmmoType then
+		pl:SetAmmo( maxClip2, secAmmoType, true)
+	end
+end
+
+
+infammo_players = {}
+
+timer.Create("igs_infammo", 1, 0, function()
+	if not infammo_players[1] then return end
+
+	for i = #infammo_players, 0, -1 do -- reversed ipairs
+		local pl = infammo_players[i]
+		if IsValid(pl) then
+			setInfAmmo(pl)
+		else
+			table.remove(infammo_players, i)
+		end
+	end
+end)
+
+function ITEM:SetInfAmmo()
+	return self:SetInstaller(function(pl)
+		if not table.HasValue(infammo_players, pl) then
+			table.insert(infammo_players, pl) 
+		end
+	end):SetValidator(function(pl)
+		return false
+	end)
+end
